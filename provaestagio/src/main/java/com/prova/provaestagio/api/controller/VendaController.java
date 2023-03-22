4f65f12865c427041cf745c251b91ad38f59bf63
Yuri Constantino Getaruck <90275932+YuriGetaruck@users.noreply.github.com>
2023-03-21 23:12:36 -0300

    Segundo Commit - primeiro dia
    Metodo POST para adicionar uma venda funcionando, assim como o GET listando as vendas no banco

--- provaestagio/src/main/java/com/prova/provaestagio/api/controller/VendaController.java
+++ provaestagio/src/main/java/com/prova/provaestagio/api/controller/VendaController.java
@@ -4,10 +4,11 @@ package com.prova.provaestagio.api.controller;
 import com.prova.provaestagio.api.repository.VendaRepository;
 import com.prova.provaestagio.api.service.SolicitacaoVendaService;
 import com.prova.provaestagio.models.Venda;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +23,14 @@ public class VendaController {
     private VendaRepository vendaRepository;
 
     @GetMapping
-    public List<Venda> listSales(){
+    public List<Venda> listVendas(){
         return vendaRepository.findAll().stream().toList();
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Venda criaVenda(@Valid @RequestBody Venda venda){
+        return solicitacaoVendaService.gerarVenda(venda);
+    }
 
 }
